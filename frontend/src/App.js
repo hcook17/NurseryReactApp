bf4d568bcde9a9b5dd8c191a529646e9c5b616b2
hcook17 <hcook6@emich.edu>
2022-05-24 16:21:43 -0400

    first try at react app

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import data from './data';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="index.html">BUSHY GARDEN</a>
+            </div>
+            <div>
+                <a href="/cart">CART</a>
+                <a href="/contact">CONTACT</a>
+                <a href="/acct">MY ACCOUNT</a>
+                <a href="/login">LOGIN</a>
+            </div>
+        </header>
+ <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,93 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Calia Lily',
+            category: 'Annual Plants',
+            image: '/images/p1.jpg',
+            price:24.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Peach, #6.5 pot'
+        },
+        {
+            _id:'2',
+            name:'Begonia',
+            category: 'Annual Plants',
+            image: '/images/p2.jpg',
+            price:14.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Peach, #6.0 pot'
+        },
+        {
+            _id:'3',
+            name:'Gardenia',
+            category: 'Annual Plants',
+            image: '/images/p3.jpg',
+            price:39.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Button, Bush'
+        },
+        {
+            _id:'4',
+            name:'Dahlia',
+            category: 'Annual Plants',
+            image: '/images/p4.jpg',
+            price: 21.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Grande Emilio'
+        },
+        {
+            _id:'5',
+            name:'Petunia',
+            category: 'Annual Plants',
+            image: '/images/p5.jpg',
+            price:19.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Purple Hanging Basket'
+        },
+        {
+            _id:'6',
+            name:'Lobelia',
+            category: 'Annual Plants',
+            image: '/images/p6.jpg',
+            price: 5.49,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Laguna Dark Blue'
+        },
+        {
+            _id:'7',
+            name:'Petunia',
+            category: 'Annual Plants',
+            image: '/images/p7.jpg',
+            price:19.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Velvet Hanging Basket'
+        },
+        {
+            _id:'8',
+            name:'Petunia',
+            category: 'Annual Plants',
+            image: '/images/p8.jpg',
+            price:19.99,
+            brand: 'Green Garden',
+            rating: 5,
+            numReviews: 10,
+            description: 'Watermelon Hanging Basket'
+        },
+    ],
+};
+export default data;
