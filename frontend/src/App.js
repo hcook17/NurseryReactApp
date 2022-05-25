c482a6f8078d05aa6692567c10c2c0e053f2ad7d
hcook17 <hcook6@emich.edu>
2022-05-25 11:52:39 -0400

    base product screen and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return ( 
-  <div>
-      <div className = "row center"> 
-      {data.products.map((product) => ( 
-          <Product key ={product._id} product={product}></Product>
-        ))} 
-      </div> 
-  </div>
-  )}
+  return (
+  <BrowserRouter>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Nursery
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">CART</a>
+          <a href="contact.html">CONTACT</a>
+          <a href="account.html">MY ACCOUNT</a>
+          <a href="signin.html">LOGIN</a>
+        </div>
+      </header>
+      <main>
+        <Routes> 
+          <Route path="/product/:id" element={<ProductScreen/>}></Route> 
+          <Route path="/" element={<HomeScreen/>} exact></Route> 
+        </Routes>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
